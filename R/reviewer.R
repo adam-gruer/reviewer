f5c96dc328ab736c306d7831920d37921c7dd530
Adam Gruer <adamgruer@icloud.com>
2018-06-25 21:56:22 +1000

    added dependencyObject

---
+++ R/reviewer.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(miniUI)
+library(ggplot2)
+
+
+mypackageDependencies <- function() {
+            htmltools::htmlDependency(name = "mypackage-assets", version = "0.1",
+                   package = "reviewer",
+                   src = "assets",
+                   script = c("scripts/jquery-3.3.1.js",
+                              "scripts/jquery-ui.js",
+                              "scripts/reviewer.js"),
+
+                   stylesheet = c("css/jquery-ui.css",
+                                  "css/jquery-ui.structure.css",
+                                  "css/jquery-ui.theme.css",
+                                  "css/reviewer.css")
+    )
+}
+
+reviewer <- function() {
+
+    ui <- miniPage(
+        mypackageDependencies(),
+        # tags$head(tags$link(rel="stylesheet", href="jquery-ui.css")),
+        # tags$head(tags$link(rel="stylesheet", href="jquery-ui.structure.css")),
+        # tags$head(tags$link(rel="stylesheet", href="jquery-ui.theme.css")),
+        # tags$head(tags$script(src="jquery-3.3.1.js")),
+        # tags$head(tags$script(src="jquery-ui.js")),
+        # tags$head(tags$script(src="reviewer.js")),
+        gadgetTitleBar("Accept or reject changes"),
+        miniContentPanel(
+
+            includeHTML("template.html")
+
+
+
+        )
+    )
+
+    server <- function(input, output, session) {
+
+
+
+        # Handle the Done button being pressed.
+        observeEvent(input$done, {
+
+            stopApp()
+        })
+    }
+
+    runGadget(ui, server)
+}
